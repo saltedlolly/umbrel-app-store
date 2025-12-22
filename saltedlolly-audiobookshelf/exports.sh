3aac1f8099bb80e8eff0b9a41ae34ee2f3441303
Olly Stedall <642592+saltedlolly@users.noreply.github.com>
2025-12-22 02:25:54 +0000

    release: v2.31.0.23 - Change locations for persistent storage. ABS config/metadata folders now should persist too, even after uninstall

--- saltedlolly-audiobookshelf/exports.sh
+++ saltedlolly-audiobookshelf/exports.sh
@@ -5,6 +5,7 @@ UMBREL_DATA_STORAGE_AUDIOBOOKS_DIR="${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_DIR}/Au
 UMBREL_DATA_STORAGE_PODCASTS_DIR="${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_DIR}/Podcasts"
 UMBREL_DATA_STORAGE_ABS_CONFIG="${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_DIR}/app-data/config"
 UMBREL_DATA_STORAGE_ABS_METADATA="${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_DIR}/app-data/metadata"
+UMBREL_DATA_STORAGE_ABS_LOGS="${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_LOGS}/app-data/metadata/logs"
 DESIRED_OWNER="1000:1000"
 
 # Ensure all Audiobookshelf directories exist
@@ -24,6 +25,10 @@ if [[ ! -d "${UMBREL_DATA_STORAGE_ABS_METADATA}" ]]; then
 	mkdir -p "${UMBREL_DATA_STORAGE_ABS_METADATA}"
 fi
 
+if [[ ! -d "${UMBREL_DATA_STORAGE_ABS_LOGS}" ]]; then
+	mkdir -p "${UMBREL_DATA_STORAGE_ABS_LOGS}"
+fi
+
 # Set correct permissions (Paperless-style approach)
 set_correct_permissions() {
 	local -r path="${1}"
@@ -32,14 +37,10 @@ set_correct_permissions() {
 		owner=$(stat -c "%u:%g" "${path}")
 
 		if [[ "${owner}" != "${DESIRED_OWNER}" ]]; then
-			chown "${DESIRED_OWNER}" "${path}"
+			chown -R "${DESIRED_OWNER}" "${path}"
 		fi
 	fi
 }
 
 set_correct_permissions "${UMBREL_DATA_STORAGE_DIR}"
 set_correct_permissions "${UMBREL_DATA_STORAGE_AUDIOBOOKSHELF_DIR}"
-set_correct_permissions "${UMBREL_DATA_STORAGE_AUDIOBOOKS_DIR}"
-set_correct_permissions "${UMBREL_DATA_STORAGE_PODCASTS_DIR}"
-set_correct_permissions "${UMBREL_DATA_STORAGE_ABS_CONFIG}"
-set_correct_permissions "${UMBREL_DATA_STORAGE_ABS_METADATA}"
